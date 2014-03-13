7bb7e1933cd3037ba928cdf3f5060c71a8c00aea
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-13 15:23:05 -0700

    Updating the minified version of the JS.

--- gs/search/base/browser/javascript/search-min.js
+++ gs/search/base/browser/javascript/search-min.js
@@ -1,4 +1,4 @@
-jQuery.noConflict();function GSSearch(e,P,E,s,N,o){var z=null,f=null,y=null,p=null,q=null,m=null,J=null,r=null,M=null,c=null,t="",G="",L=true,w=false,A="slow",g="swing",i="resultsloaded";
+"use strict";jQuery.noConflict();function GSSearch(e,P,E,s,N,o){var z=null,f=null,y=null,p=null,q=null,m=null,J=null,r=null,M=null,c=null,t="",G="",L=true,w=false,A="slow",g="swing",i="resultsloaded";
 function d(Q){Q.attr("disabled","disabled");Q.attr("aria-disabled","true")}function h(Q){Q.removeAttr("disabled");
 Q.attr("aria-disabled","false")}function C(){J.on("click",j)}function j(Q){E=E-s;
 if(E<0){E=0}B()}function b(){r.on("click",D)}function D(Q){var R=null;if(f.val()){E=E+s
@@ -15,11 +15,11 @@ q.attr("aria-hidden","false");p.height("auto");h(f);h(y);if(E<=0){d(J)}else{h(J)
 if(Q<s){d(r)}else{h(r)}l();if((E<=0)&&(Q<s)&&L){m.fadeOut("fast",g);L=false}else{if(((E>0)||(Q>=s))&&!L){m.fadeIn("fast",g);
 L=true}}if(Q==0){if(f.val()===""){c.fadeIn("fast",g);f.fadeOut("fast",g);y.fadeOut("fast",g)
 }else{M.fadeIn("fast",g)}if(q.is(":visible")){q.hide()}}x();w=true}function x(){var Q=null;
-Q=jQuery.Event(i);z.trigger(Q)}function l(){keywords=null;keywords=q.find(".gs-search-keyword");
-if(keywords.length>0){keywords.removeAttr("href").css("cursor","pointer");keywords.click(n)
-}}function n(Q){var R=jQuery(this).text();f.val(R);y.click()}function K(){z=jQuery(e);
-f=z.find(".gs-search-entry input");F();y=z.find(".gs-search-entry button");H();p=z.find(".gs-search-loading");
-q=z.find(".gs-search-results");m=z.find(".gs-search-toolbar");J=z.find(".gs-search-toolbar-previous");
-C();r=z.find(".gs-search-toolbar-next");b();c=z.find(".gs-search-empty");c.hide();
-M=z.find(".gs-search-failed");M.hide();t=P}K();return{load:function(){y.click()},results_shown:function(){return w
-},RESULTS_LOADED_EVENT:i}};
+Q=jQuery.Event(i);z.trigger(Q)}function l(){var Q=null;Q=q.find(".gs-search-keyword");
+if(Q.length>0){Q.removeAttr("href").css("cursor","pointer");Q.click(n)}}function n(Q){var R=jQuery(this).text();
+f.val(R);y.click()}function K(){z=jQuery(e);f=z.find(".gs-search-entry input");F();
+y=z.find(".gs-search-entry button");H();p=z.find(".gs-search-loading");q=z.find(".gs-search-results");
+m=z.find(".gs-search-toolbar");J=z.find(".gs-search-toolbar-previous");C();r=z.find(".gs-search-toolbar-next");
+b();c=z.find(".gs-search-empty");c.hide();M=z.find(".gs-search-failed");M.hide();
+t=P}K();return{load:function(){y.click()},results_shown:function(){return w},RESULTS_LOADED_EVENT:i}
+};
